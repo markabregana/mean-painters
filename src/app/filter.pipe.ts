92839cce420da4f6fdc4229bfaadf3f2856e1b4b
Mark Abregana <mark.abregana@gmail.com>
2017-09-04 11:58:55 +0800

    updated filter pipe

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -7,7 +7,7 @@ export class FilterPipe implements PipeTransform {
 
   transform(items: any, term: any, value: any): any[] {
     if (!items) { return []; }
-    console.log(term + ' - ' + value);
+    // console.log(term + ' - ' + value);
     return items.filter(it => it[term] === value);
   }
 
